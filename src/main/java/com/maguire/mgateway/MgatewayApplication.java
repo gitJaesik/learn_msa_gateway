b4540b9d804718d7646a672c55802e7297900422
gitJaesik <maguire1815@gmail.com>
2018-09-09 09:42:43 +0900

    Initial commit, empty project

---
+++ src/main/java/com/maguire/mgateway/MgatewayApplication.java
@@ -0,0 +1,12 @@
+package com.maguire.mgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgatewayApplication.class, args);
+	}
+}
